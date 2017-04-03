0af5fe7f97d757b5615d30047a90e7d8a3b7a4c4
Parallels <jdpeven@gmail.com>
2017-04-02 22:22:44 -0700

    Something bad is happenign with debug

--- mkdir_creat.c
+++ mkdir_creat.c
@@ -2,6 +2,112 @@
 #include "util.c"
 #include "type.h"
 
+
+enterChild(MINODE * pmip, int ino, char * basename)
+{
+    int needLength;
+    int parentBlockNum = 0;
+    char bbuf[BLKSIZE];
+    char * cp
+
+    printf("Entering enterChild\n");
+
+    needLength = 4*((8+strlen(basename)+3)/4);
+    //Now actually adding the dir to the block
+    while(parent->INODE.i_block[parentBlockNum+1] != 0)             //finding last non-empty block
+        parentBlockNum++;
+    printf("First available block is at index [%d] with Block # [%d]\n",parentBlockNum, parent->INODE.i_block[parentBlockNum]);
+
+    getblock(parent->dev, parent->INODE.i_block[parentBlockNum],bbuf);
+
+    dp = (DIR *)bbuf;
+    cp = bbuf;
+
+    if(dp->inode != 0)                              //not the first entry in that dirblock
+    {
+        printf("Step to last data entry in block: [%d]\n", parent->INODE.i_block[parentBlockNum]);
+
+        while(cp + dp->rec_len < bbuf[BLKSIZE])
+        {
+            printf("%4d %4d %4d %s\n", dp->inode, dp->rec_len, dp->name_len, sbuf);//printing info
+            if(dp->rec_len == 4*((8+dp->name_len+3)/4))             //normal entries
+            {
+                cp+=dp->rec_len;
+                dp = (DIR *)cp;
+            }
+            else                                        //we've reached the end
+            {
+                if((4*((8+dp->name_len+3)/4)) + needLength > dp->rec_len)   //if the actual size of the current last inode is greater than
+                                                                            //what it needs AND our new file, then we need to allocate a new
+                                                                            //dirblock
+                {
+                    printf("Need to allocate new dir block\n");
+                    break;
+                    //TODO
+                    //that
+                }
+                //otherwise it will fit into this block
+                remainLength = dp->rec_len - (4*((8+dp->name_len+3)/4));
+                dp->rec_len = (4*((8+dp->name_len+3)/4));
+                break;
+            }
+        }
+    }
+    //else, first entry in datablock, dp is already pointing to the right location
+    strcpy(dp->name, basename);
+    dp->inode = ino;
+    dp->rec_len = remainLength;
+    dp->name_len = strlen(basename);
+}
+
+kmkdir(MINODE * pmip, char * basename)
+{
+    int ino, bno, i;
+    char * buf[BLKSIZE];
+    char * cp;
+    DIR *mdp = malloc(sizeof(DIR));
+
+    MINODE * mip = malloc(sizeof(MINODE));
+    DIR * newdp = malloc(sizeof(DIR));
+    
+    printf("Entering kmkdir\n");
+
+    ino = ialloc(pmip->cwd->dev);
+    bno = balloc(pmip->cwd->dev);
+    mip = iget(pmip->cwd->dev, ino);
+
+    INODE * ip = &mip->INODE;
+    ip->i_mode = 0x41ED;
+    ip->i_uid = running->uid;
+    ip->i_gid = running->gid;
+    ip->i_size = BLKSIZE;
+    ip->i_links_count = 2;
+    ip->i_atime = i_ctime = i_mtime = time(0L);            //THIS IS RELEVANT FOR TOUCH
+    ip->i_blocks = 2;
+    ip->i_block[0] = bno;
+    for(i = 0; i < 15; i++)
+        ip->i_block[i] = 0;
+    mip->dirty = 1;
+    iput(mip);
+
+    cp = &buf;
+    mdp = (DIR * ) buf;
+    strcpy(mdp->name, ".");
+    mdp->inode = ino;
+    mdp->name_len = 1;
+    mdp->rec_len = 4*((8+strlen(".")+3)/4);
+    cp += mdp->rec_len;
+    mdp = (DIR *)cp;
+    strcpy(mdp->name, "..");
+    mdp->inode = pmip->ino;
+    mdp->name_len = 2;
+    mdp->rec_len = 4*((8+strlen("..")+3)/4);
+
+    putblock(pmip->cwd->dev, bno, buf);
+
+    enterChild(pmip, ino, basename);
+}
+
 int mymkdir(char * pathname)
 {
     MINODE *parent = malloc(sizeof(MINODE));
@@ -67,106 +173,6 @@ int mymkdir(char * pathname)
     iput(parent);
     return 0;
 
-}
-
-kmkdir(MINODE * pmip, char * basename)
-{
-    int ino, bno, i;
-    char * buf[BLKSIZE];
-    char * cp;
-    DIR *mdp = malloc(sizeof(DIR));
-
-    MINODE * mip = malloc(sizeof(MINODE));
-    DIR * newdp = malloc(sizeof(DIR));
-    
-    ino = ialloc(pmip->cwd->dev);
-    bno = balloc(pmip->cwd->dev);
-    mip = iget(pmip->cwd->dev, ino);
-
-    INODE * ip = &mip->INODE;
-    ip->i_mode = 0x41ED;
-    ip->i_uid = running->uid;
-    ip->i_gid = running->gid;
-    ip->i_size = BLKSIZE;
-    ip->i_links_count = 2;
-    ip->i_atime = i_ctime = i_mtime = time(0L);            //THIS IS RELEVANT FOR TOUCH
-    ip->i_blocks = 2;
-    ip->i_block[0] = bno;
-    for(i = 0; i < 15; i++)
-        ip->i_block[i] = 0;
-    mip->dirty = 1;
-    iput(mip);
-
-    cp = &buf;
-    mdp = (DIR * ) buf;
-    strcpy(mdp->name, ".");
-    mdp->inode = ino;
-    mdp->name_len = 1;
-    mdp->rec_len = 4*((8+strlen(".")+3)/4);
-    cp += mdp->rec_len;
-    mdp = (DIR *)cp;
-    strcpy(mdp->name, "..");
-    mdp->inode = pmip->ino;
-    mdp->name_len = 2;
-    mdp->rec_len = 4*((8+strlen("..")+3)/4);
-
-    putblock(pmip->cwd->dev, bno, buf);
-
-    enterChild(pmip, ino, basename);
-}
-
-enterChild(MINODE * pmip, int ino, char * basename)
-{
-    int needLength;
-    int parentBlockNum = 0;
-    char bbuf[BLKSIZE];
-    char * cp
-    needLength = 4*((8+strlen(basename)+3)/4);
-    //Now actually adding the dir to the block
-    while(parent->INODE.i_block[parentBlockNum+1] != 0)             //finding last non-empty block
-        parentBlockNum++;
-    printf("First available block is at index [%d] with Block # [%d]\n",parentBlockNum, parent->INODE.i_block[parentBlockNum]);
-
-    getblock(parent->dev, parent->INODE.i_block[parentBlockNum],bbuf);
-
-    dp = (DIR *)bbuf;
-    cp = bbuf;
-
-    if(dp->inode != 0)                              //not the first entry in that dirblock
-    {
-        printf("Step to last data entry in block: [%d]\n", parent->INODE.i_block[parentBlockNum]);
-
-        while(cp + dp->rec_len < bbuf[BLKSIZE])
-        {
-            printf("%4d %4d %4d %s\n", dp->inode, dp->rec_len, dp->name_len, sbuf);//printing info
-            if(dp->rec_len == 4*((8+dp->name_len+3)/4))             //normal entries
-            {
-                cp+=dp->rec_len;
-                dp = (DIR *)cp;
-            }
-            else                                        //we've reached the end
-            {
-                if((4*((8+dp->name_len+3)/4)) + needLength > dp->rec_len)   //if the actual size of the current last inode is greater than
-                                                                            //what it needs AND our new file, then we need to allocate a new
-                                                                            //dirblock
-                {
-                    printf("Need to allocate new dir block\n");
-                    break;
-                    //TODO
-                    //that
-                }
-                //otherwise it will fit into this block
-                remainLength = dp->rec_len - (4*((8+dp->name_len+3)/4));
-                dp->rec_len = (4*((8+dp->name_len+3)/4));
-                break;
-            }
-        }
-    }
-    //else, first entry in datablock, dp is already pointing to the right location
-    strcpy(dp->name, basename);
-    dp->inode = ino;
-    dp->rec_len = remainLength;
-    dp->name_len = strlen(basename);
 }
 
 /************* Algorithm of Insert_dir_entry ***************** 
