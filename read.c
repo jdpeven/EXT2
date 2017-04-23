cfb8eb488278146ebf0969ece691547c4b1d694e
DillonPatrickJohnson <dillon.p.johnson@gmail.com>
2017-04-23 11:36:05 -0700

    fucking problems...

--- read.c
+++ read.c
@@ -9,8 +9,9 @@
 int myread (char* filename, char* bytesToRead)
 {
 	MINODE *mip;
-	int i, inoToFind = 0, curfd = -1;
-	char buf[BLKSIZE];
+	int i, inoToFind = 0, curfd = -1, bytesRead;
+	//char buf[BLKSIZE];
+	char* buf;
 	int btoread = atoi(bytesToRead);
 
 	strcpy(buf, "");
@@ -44,28 +45,72 @@ int myread (char* filename, char* bytesToRead)
 	i = 0;
 	while (mip->INODE.i_block[i] != 0)
 	{
-		read_block(mip->dev, mip->INODE.i_block[i], buf, btoread);
-		printf("Read %d bytes\n%s\n", btoread, buf);
-		buf[0] = 0;
+		bytesRead = read_block(curfd, mip->INODE.i_block[i], buf, btoread);
+		printf("Read %d bytes\n%s\n", bytesRead, buf);
 		i++;
 	}
 }
 
-int read_block(int fd, char buf[ ], int size)
+int read_block(int fd, char *buf, int nbytes)
 {
-	int offset, mode, refCount, avil;
+	int offset, mode, refCount, avil, lbk, startByte, blk, remain, bytesRead = 0;
+	char *cq = buf, *readbuf, *cp;
 
 	printf("entered read_block\n");
 
 	offset = running->fd[fd]->offset;
-	printf("entered read_block\n");
 	mode = running->fd[fd]->mode;
-	printf("entered read_block\n");
 	refCount = running->fd[fd]->refCount;
-	printf("entered read_block\n");
 	avil = running->fd[fd]->mptr->INODE.i_size - offset;
 
 	printf("offset: %d\nmode: %d\nrefCount: %d\navil: %d\n", offset, mode, refCount, avil);
+
+	while (nbytes && avil)
+	{
+		lbk = offset / BLKSIZE;
+		startByte = offset % BLKSIZE;
+
+		printf("lbk = %d\nstartByte = %d\n", lbk, startByte);
+
+		if (lbk < 12)
+		{
+			printf("reading from a DIRECT block\n");
+			blk = running->fd[fd]->mptr->INODE.i_block[lbk];
+		}
+		else if (lbk >= 12 && lbk < 256 + 12)
+		{
+			printf("reading from a INDIRECT block\n");
+			//indirect
+		}
+		else
+		{
+			printf("reading from a 2xINDIRECT block\n");
+			//2x indirect
+		}
+
+		get_block(running->fd[fd]->mptr->dev, blk, readbuf);
+		printf("block got got\n");
+
+		cp = readbuf + startByte;
+		remain = BLKSIZE - startByte;
+		while (remain > 0)
+		{
+			printf("remain = %d\n", remain);
+			*cq++ = *cp++;
+			printf("not asdfk;lahj\n");
+			running->fd[fd]->offset++;
+			bytesRead++;
+			avil--;
+			nbytes--;
+			remain--;
+			if (nbytes <= 0 || avil <= 0)
+			{
+				break;
+			}
+		}
+	}
+	printf("read_block: read %d char from fd %d\n", bytesRead, fd);
+	return bytesRead;
 }
 
 #endif
